e0349294cfa2322cbd4caeafc0f97980fb65e5dd
SiddharthBharadwaj <siddharthbharadwaj45@gmail.com>
2020-04-22 09:15:42 +0000

    x2: Update Device Identification Props

--- init/init_x2.cpp
+++ init/init_x2.cpp
@@ -1,7 +1,30 @@
 /*
- * Copyright (C) 2020 AOSP Project
- *
- * SPDX-License-Identifier: Apache-2.0
+   Copyright (C) 2017-2018 The Android Open Source Project
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <cstdlib>
@@ -20,20 +43,70 @@ using namespace std;
 namespace android {
 namespace init {
 
-bool isCN(){
-  // Get region
-  std::ifstream infile("/proc/oppoVersion/region"); std::string region;
-  bool ret = false;
-  getline(infile, region);
-  if(!region.compare("China"))
-      ret = true; 
-  return ret;
+static bool file_exist (const std::string& name) {
+    return ( access( name.c_str(), F_OK ) == 0 );
+}
+
+void load_CN() {
+    property_set("ro.product.device", "RMX1991CN");
+    property_set("ro.product.name", "RMX1991");
+    property_set("ro.build.product", "RMX1991");
+    property_set("ro.separate.soft", "19771");
+}
+
+void load_EU() {
+    property_set("ro.product.device", "RMX1993");
+    property_set("ro.product.name", "RMX1993");
+    property_set("ro.build.product", "RMX1993");
+    property_set("ro.separate.soft", "19672");
+}
+
+void load_IN() {
+    property_set("ro.product.device", "RMX1992");
+    property_set("ro.product.name", "RMX1992");
+    property_set("ro.build.product", "RMX1992");
+    property_set("ro.separate.soft", "19671");
 }
 
 void vendor_load_properties() {
-  property_set("ro.product.device", isCN()   ?  "RMX1991CN"   :   "RMX1992");
-  property_set("ro.product.name",   isCN()   ?  "RMX1991"     :   "RMX1992");
-  property_set("ro.build.product",  isCN()   ?  "RMX1991"     :   "RMX1992"); 
+    const char* path = "/proc/partitions";
+	std::ifstream infile(path);
+	std::string line;
+	while (std::getline(infile, line))
+	{
+		if (line.find("sda7") != string::npos)
+		{
+			if (line.substr(17, 7) == "4218880") {
+				load_CN();
+				break;
+			}
+			else if (line.substr(17, 7) == "5557452") {
+                const char* pathnfc = "/proc/touchpanel/NFC_CHECK";
+                if (file_exist(pathnfc)){
+                    std::ifstream infilenfc(pathnfc);
+                    std::string linenfc;
+                    std::getline(infilenfc, linenfc);
+                    if(linenfc.compare("SUPPORTED") == 0){
+                        load_EU();
+                        break;
+                    } else {
+                        load_IN();
+                        break;
+                    }
+
+                } else {
+				    load_EU();
+				    break;
+                }
+			}
+			else
+			{
+				load_CN();
+				break;
+			}
+		}
+	}
 }
+
 }  // namespace init
 }  // namespace android
